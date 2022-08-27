80939b3a85ba10a0489fd17f6665cd162b5ad615
meharmohal <112121917+meharmohal@users.noreply.github.com>
2022-08-27 12:03:47 -0400

    Initial Commit

---
+++ Biblo/BibloApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibloApp.swift
+//  Biblo
+//
+//  Created by Mehar Mohal on 2022-08-27.
+//
+
+import SwiftUI
+
+@main
+struct BibloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Biblo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Biblo
+//
+//  Created by Mehar Mohal on 2022-08-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
