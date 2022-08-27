c632e3df51c8972dbf316c7d4061673c06caf09c
meharmohal <112121917+meharmohal@users.noreply.github.com>
2022-08-27 12:41:45 -0400

    hello world start

---
+++ Biblo/Core/Feed/View/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  Biblo
+//
+//  Created by Mehar Mohal on 2022-08-27.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
